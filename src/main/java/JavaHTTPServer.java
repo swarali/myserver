0eb0fb28e4c16353e7d99e1c6604588fcfab7ab5
swarali <swarakarkhanis@gmail.com>
2020-02-24 05:40:26 +0100

    Add some sample html files

--- src/main/java/JavaHTTPServer.java
+++ src/main/java/JavaHTTPServer.java
@@ -1,13 +1,5 @@
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Date;
@@ -19,7 +11,7 @@ import java.util.StringTokenizer;
 // Each Client Connection will be managed in a dedicated Thread
 public class JavaHTTPServer implements Runnable{
 
-    static final File WEB_ROOT = new File(".");
+    static final File WEB_ROOT = new File(ClassLoader.getSystemResource("").getPath());
     static final String DEFAULT_FILE = "index.html";
     static final String FILE_NOT_FOUND = "404.html";
     static final String METHOD_NOT_SUPPORTED = "not_supported.html";
